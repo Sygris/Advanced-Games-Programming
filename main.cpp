e111c32b404ebfa573ede455b542c10537b82366
Tiago Antunes <tiago.a.vista@gmail.com>
2021-12-20 11:00:05 +0000

    test

--- main.cpp
+++ main.cpp
@@ -5,20 +5,20 @@
 
 int WINAPI WinMain(_In_ HINSTANCE hInst, _In_opt_ HINSTANCE hPrev, _In_ LPSTR lpCmd, _In_ int nCmdShow)
 {
-	D3D11CreateDeviceAndSwapChain(
-		NULL,
-		D3D_DRIVER_TYPE_REFERENCE,
-		NULL,
-		NULL,
-		NULL,
-		NULL,
-		D3D11_SDK_VERSION,
-		NULL,
-		NULL,
-		NULL,
-		NULL,
-		NULL
-	);
+	//D3D11CreateDeviceAndSwapChain(
+	//	NULL,
+	//	D3D_DRIVER_TYPE_REFERENCE,
+	//	NULL,
+	//	NULL,
+	//	NULL,
+	//	NULL,
+	//	D3D11_SDK_VERSION,
+	//	NULL,
+	//	NULL,
+	//	NULL,
+	//	NULL,
+	//	NULL
+	//);
 
 	MessageBox(NULL, L"Hello World!", L"Tiago Antunes Boa Vista", MB_OK);
 
